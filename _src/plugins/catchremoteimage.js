415035f6043f8e17df7c2c9d4c8109c9b88d607b
ModStart <modstart@163.com>
2023-10-25 09:36:22 +0800

    feat: 移除部分list无效代码，loading类名统一前缀uep

--- _src/plugins/catchremoteimage.js
+++ _src/plugins/catchremoteimage.js
@@ -6,194 +6,194 @@
  * 远程图片抓取,当开启本插件时所有不符合本地域名的图片都将被抓取成为本地服务器上的图片
  */
 UE.plugins["catchremoteimage"] = function () {
-  var me = this,
-    ajax = UE.ajax;
+    var me = this,
+        ajax = UE.ajax;
 
-  /* 设置默认值 */
-  if (me.options.catchRemoteImageEnable === false) {
-    return;
-  }
-  me.setOpt({
-    catchRemoteImageEnable: false
-  });
-
-  var catcherLocalDomain = me.getOpt("catcherLocalDomain"),
-    catcherActionUrl = me.getActionUrl(me.getOpt("catcherActionName")),
-    catcherUrlPrefix = me.getOpt("catcherUrlPrefix"),
-    catcherFieldName = me.getOpt("catcherFieldName");
-
-  me.addListener('serverConfigLoaded', function () {
-    catcherLocalDomain = me.getOpt("catcherLocalDomain");
-    catcherActionUrl = me.getActionUrl(me.getOpt("catcherActionName"));
-    catcherUrlPrefix = me.getOpt("catcherUrlPrefix");
-    catcherFieldName = me.getOpt("catcherFieldName");
-  });
-
-  me.addListener("afterpaste", function () {
-    me.fireEvent("catchremoteimage");
-  });
-
-  var catchRemoteImageCatching = false;
-
-  function sendApi(imgs, callbacks) {
-    var params = utils.serializeParam(me.queryCommandValue("serverparam")) || "",
-      url = utils.formatUrl(
-        catcherActionUrl +
-        (catcherActionUrl.indexOf("?") === -1 ? "?" : "&") +
-        params
-      ),
-      isJsonp = utils.isCrossDomainUrl(url),
-      opt = {
-        method: "POST",
-        dataType: isJsonp ? "jsonp" : "",
-        timeout: 60000, //单位：毫秒，回调请求超时设置。目标用户如果网速不是很快的话此处建议设置一个较大的数值
-        headers: me.options.serverHeaders || {},
-        onsuccess: callbacks["success"],
-        onerror: callbacks["error"]
-      };
-    opt[catcherFieldName] = imgs;
-    ajax.request(url, opt);
-  }
-
-  function catchElement(type, ele, imageUrl) {
-    sendApi([imageUrl], {
-      //成功抓取
-      success: function (r) {
-        try {
-          var info = r.state !== undefined
-            ? r
-            : eval("(" + r.responseText + ")");
-        } catch (e) {
-          return;
-        }
-
-        /* 获取源路径和新路径 */
-        var oldSrc,
-          newSrc,
-          oldBgIMG,
-          newBgIMG,
-          list = info.list;
-        var catchFailList = [];
-        var catchSuccessList = [];
-        var failIMG = me.options.themePath + me.options.theme + '/images/img-cracked.png';
-        var loadingIMG = me.options.themePath + me.options.theme + '/images/spacer.gif';
-
-        var cj = list[0];
-        switch (type) {
-          case 'image':
-            oldSrc = ele.getAttribute("_src") || ele.src || "";
-            if (cj.state === "SUCCESS") {
-              newSrc = catcherUrlPrefix + cj.url;
-              // 上传成功是删除uploading动画
-              domUtils.removeClasses(ele, "loadingclass");
-              domUtils.setAttributes(ele, {
-                "src": newSrc,
-                "_src": newSrc,
-                "data-catch-result": "success"
-              });
-              catchSuccessList.push(ele);
-            } else {
-              // 替换成统一的失败图片
-              domUtils.removeClasses(ele, "loadingclass");
-              domUtils.setAttributes(ele, {
-                "src": failIMG,
-                "_src": failIMG,
-                "data-catch-result": "fail" // 添加catch失败标记
-              });
-              catchFailList.push(ele);
-            }
-            break;
-          case 'background':
-            oldBgIMG = ele.getAttribute("data-background") || "";
-            if (cj.state === "SUCCESS") {
-              newBgIMG = catcherUrlPrefix + cj.url;
-              ele.style.cssText = ele.style.cssText.replace(loadingIMG, newBgIMG);
-              domUtils.removeAttributes(ele, "data-background");
-              domUtils.setAttributes(ele, {
-                "data-catch-result": "success"   // 添加catch成功标记
-              });
-              catchSuccessList.push(ele);
-            } else {
-              ele.style.cssText = ele.style.cssText.replace(loadingIMG, failIMG);
-              domUtils.removeAttributes(ele, "data-background");
-              domUtils.setAttributes(ele, {
-                "data-catch-result": "fail"   // 添加catch失败标记
-              });
-              catchFailList.push(ele);
-            }
-            break;
-        }
-        // 监听事件添加成功抓取和抓取失败的dom列表参数
-        me.fireEvent('catchremotesuccess', catchSuccessList, catchFailList);
-        catchRemoteImageCatching = false;
-        setTimeout(function () {
-          me.fireEvent('catchremoteimage');
-        }, 0);
-      },
-      //回调失败，本次请求超时
-      error: function () {
-        me.fireEvent('catchremoteerror');
-        catchRemoteImageCatching = false;
-        setTimeout(function () {
-          me.fireEvent('catchremoteimage');
-        }, 0);
-      }
+    /* 设置默认值 */
+    if (me.options.catchRemoteImageEnable === false) {
+        return;
+    }
+    me.setOpt({
+        catchRemoteImageEnable: false
     });
-  }
 
-  function catchRemoteImage() {
-    if (catchRemoteImageCatching) {
-      return;
+    var catcherLocalDomain = me.getOpt("catcherLocalDomain"),
+        catcherActionUrl = me.getActionUrl(me.getOpt("catcherActionName")),
+        catcherUrlPrefix = me.getOpt("catcherUrlPrefix"),
+        catcherFieldName = me.getOpt("catcherFieldName");
+
+    me.addListener('serverConfigLoaded', function () {
+        catcherLocalDomain = me.getOpt("catcherLocalDomain");
+        catcherActionUrl = me.getActionUrl(me.getOpt("catcherActionName"));
+        catcherUrlPrefix = me.getOpt("catcherUrlPrefix");
+        catcherFieldName = me.getOpt("catcherFieldName");
+    });
+
+    me.addListener("afterpaste", function () {
+        me.fireEvent("catchremoteimage");
+    });
+
+    var catchRemoteImageCatching = false;
+
+    function sendApi(imgs, callbacks) {
+        var params = utils.serializeParam(me.queryCommandValue("serverparam")) || "",
+            url = utils.formatUrl(
+                catcherActionUrl +
+                (catcherActionUrl.indexOf("?") === -1 ? "?" : "&") +
+                params
+            ),
+            isJsonp = utils.isCrossDomainUrl(url),
+            opt = {
+                method: "POST",
+                dataType: isJsonp ? "jsonp" : "",
+                timeout: 60000, //单位：毫秒，回调请求超时设置。目标用户如果网速不是很快的话此处建议设置一个较大的数值
+                headers: me.options.serverHeaders || {},
+                onsuccess: callbacks["success"],
+                onerror: callbacks["error"]
+            };
+        opt[catcherFieldName] = imgs;
+        ajax.request(url, opt);
     }
-    catchRemoteImageCatching = true;
 
-    var loadingIMG = me.options.themePath + me.options.theme + '/images/spacer.gif',
-      imgs = me.document.querySelectorAll('[style*="url"],img'),
-      test = function (src, urls) {
-        if (src.indexOf(location.host) !== -1 || /(^\.)|(^\/)/.test(src)) {
-          return true;
-        }
-        if (urls) {
-          for (var j = 0, url; (url = urls[j++]);) {
-            if (src.indexOf(url) !== -1) {
-              return true;
+    function catchElement(type, ele, imageUrl) {
+        sendApi([imageUrl], {
+            //成功抓取
+            success: function (r) {
+                try {
+                    var info = r.state !== undefined
+                        ? r
+                        : eval("(" + r.responseText + ")");
+                } catch (e) {
+                    return;
+                }
+
+                /* 获取源路径和新路径 */
+                var oldSrc,
+                    newSrc,
+                    oldBgIMG,
+                    newBgIMG,
+                    list = info.list;
+                var catchFailList = [];
+                var catchSuccessList = [];
+                var failIMG = me.options.themePath + me.options.theme + '/images/img-cracked.png';
+                var loadingIMG = me.options.themePath + me.options.theme + '/images/spacer.gif';
+
+                var cj = list[0];
+                switch (type) {
+                    case 'image':
+                        oldSrc = ele.getAttribute("_src") || ele.src || "";
+                        if (cj.state === "SUCCESS") {
+                            newSrc = catcherUrlPrefix + cj.url;
+                            // 上传成功是删除uploading动画
+                            domUtils.removeClasses(ele, "uep-loading");
+                            domUtils.setAttributes(ele, {
+                                "src": newSrc,
+                                "_src": newSrc,
+                                "data-catch-result": "success"
+                            });
+                            catchSuccessList.push(ele);
+                        } else {
+                            // 替换成统一的失败图片
+                            domUtils.removeClasses(ele, "uep-loading");
+                            domUtils.setAttributes(ele, {
+                                "src": failIMG,
+                                "_src": failIMG,
+                                "data-catch-result": "fail" // 添加catch失败标记
+                            });
+                            catchFailList.push(ele);
+                        }
+                        break;
+                    case 'background':
+                        oldBgIMG = ele.getAttribute("data-background") || "";
+                        if (cj.state === "SUCCESS") {
+                            newBgIMG = catcherUrlPrefix + cj.url;
+                            ele.style.cssText = ele.style.cssText.replace(loadingIMG, newBgIMG);
+                            domUtils.removeAttributes(ele, "data-background");
+                            domUtils.setAttributes(ele, {
+                                "data-catch-result": "success"   // 添加catch成功标记
+                            });
+                            catchSuccessList.push(ele);
+                        } else {
+                            ele.style.cssText = ele.style.cssText.replace(loadingIMG, failIMG);
+                            domUtils.removeAttributes(ele, "data-background");
+                            domUtils.setAttributes(ele, {
+                                "data-catch-result": "fail"   // 添加catch失败标记
+                            });
+                            catchFailList.push(ele);
+                        }
+                        break;
+                }
+                // 监听事件添加成功抓取和抓取失败的dom列表参数
+                me.fireEvent('catchremotesuccess', catchSuccessList, catchFailList);
+                catchRemoteImageCatching = false;
+                setTimeout(function () {
+                    me.fireEvent('catchremoteimage');
+                }, 0);
+            },
+            //回调失败，本次请求超时
+            error: function () {
+                me.fireEvent('catchremoteerror');
+                catchRemoteImageCatching = false;
+                setTimeout(function () {
+                    me.fireEvent('catchremoteimage');
+                }, 0);
             }
-          }
-        }
-        return false;
-      };
-
-    for (var i = 0, ci; (ci = imgs[i++]);) {
-      if (ci.getAttribute("data-word-image") || ci.getAttribute('data-catch-result')) {
-        continue;
-      }
-      if (ci.nodeName === "IMG") {
-        var src = ci.getAttribute("_src") || ci.src || "";
-        if (/^(https?|ftp):/i.test(src) && !test(src, catcherLocalDomain)) {
-          catchElement('image', ci, src);
-          domUtils.setAttributes(ci, {
-            class: "loadingclass",
-            _src: src,
-            src: loadingIMG
-          })
-          return;
-        }
-      } else {
-        var backgroundImageurl = ci.style.cssText.replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
-        if (/^(https?|ftp):/i.test(backgroundImageurl) && !test(backgroundImageurl, catcherLocalDomain)) {
-          catchElement('background', ci, backgroundImageurl);
-          ci.style.cssText = ci.style.cssText.replace(backgroundImageurl, loadingIMG);
-          domUtils.setAttributes(ci, {
-            "data-background": backgroundImageurl
-          })
-          return;
-        }
-      }
+        });
     }
 
-  };
+    function catchRemoteImage() {
+        if (catchRemoteImageCatching) {
+            return;
+        }
+        catchRemoteImageCatching = true;
 
-  me.addListener("catchremoteimage", function () {
-    catchRemoteImage();
-  });
+        var loadingIMG = me.options.themePath + me.options.theme + '/images/spacer.gif',
+            imgs = me.document.querySelectorAll('[style*="url"],img'),
+            test = function (src, urls) {
+                if (src.indexOf(location.host) !== -1 || /(^\.)|(^\/)/.test(src)) {
+                    return true;
+                }
+                if (urls) {
+                    for (var j = 0, url; (url = urls[j++]);) {
+                        if (src.indexOf(url) !== -1) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            };
+
+        for (var i = 0, ci; (ci = imgs[i++]);) {
+            if (ci.getAttribute("data-word-image") || ci.getAttribute('data-catch-result')) {
+                continue;
+            }
+            if (ci.nodeName === "IMG") {
+                var src = ci.getAttribute("_src") || ci.src || "";
+                if (/^(https?|ftp):/i.test(src) && !test(src, catcherLocalDomain)) {
+                    catchElement('image', ci, src);
+                    domUtils.setAttributes(ci, {
+                        class: "loadingclass",
+                        _src: src,
+                        src: loadingIMG
+                    })
+                    return;
+                }
+            } else {
+                var backgroundImageurl = ci.style.cssText.replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
+                if (/^(https?|ftp):/i.test(backgroundImageurl) && !test(backgroundImageurl, catcherLocalDomain)) {
+                    catchElement('background', ci, backgroundImageurl);
+                    ci.style.cssText = ci.style.cssText.replace(backgroundImageurl, loadingIMG);
+                    domUtils.setAttributes(ci, {
+                        "data-background": backgroundImageurl
+                    })
+                    return;
+                }
+            }
+        }
+
+    };
+
+    me.addListener("catchremoteimage", function () {
+        catchRemoteImage();
+    });
 };
--- _src/plugins/simpleupload.js
+++ _src/plugins/simpleupload.js
@@ -5,148 +5,148 @@
  * @date 2014-03-31
  */
 UE.plugin.register("simpleupload", function () {
-  var me = this,
-    isLoaded = false,
-    containerBtn;
+    var me = this,
+        isLoaded = false,
+        containerBtn;
 
-  function initUploadBtn() {
-    var input = document.createElement("input");
-    input.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;cursor:pointer;font-size:0;opacity:0;';
-    input.type = 'file';
-    containerBtn.appendChild(input);
-    domUtils.on(input,'change',function (e) {
-      var state = me.queryCommandState("simpleupload");
-      if (state === -1) {
-        return;
-      }
-      if (!input.value) {
-        return;
-      }
+    function initUploadBtn() {
+        var input = document.createElement("input");
+        input.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;cursor:pointer;font-size:0;opacity:0;';
+        input.type = 'file';
+        containerBtn.appendChild(input);
+        domUtils.on(input, 'change', function (e) {
+            var state = me.queryCommandState("simpleupload");
+            if (state === -1) {
+                return;
+            }
+            if (!input.value) {
+                return;
+            }
 
-      var loadingId = UE.dialog.loadingPlaceholder(me);
+            var loadingId = UE.dialog.loadingPlaceholder(me);
 
-      if (!me.getOpt("imageActionName")) {
-        UE.dialog.removeLoadingPlaceholder(me, loadingId);
-        UE.dialog.tipError(me, me.getLang("autoupload.errorLoadConfig"));
-        return;
-      }
+            if (!me.getOpt("imageActionName")) {
+                UE.dialog.removeLoadingPlaceholder(me, loadingId);
+                UE.dialog.tipError(me, me.getLang("autoupload.errorLoadConfig"));
+                return;
+            }
 
-      var allowFiles = me.getOpt("imageAllowFiles");
-      var filename = input.value, fileext = filename ? filename.substr(filename.lastIndexOf(".")) : "";
-      if (
-        !fileext ||
-        (allowFiles &&
-          (allowFiles.join("") + ".").indexOf(fileext.toLowerCase() + ".") === -1)
-      ) {
-        UE.dialog.removeLoadingPlaceholder(me, loadingId);
-        UE.dialog.tipError(me, me.getLang("autoupload.exceedTypeError"));
-        return;
-      }
+            var allowFiles = me.getOpt("imageAllowFiles");
+            var filename = input.value, fileext = filename ? filename.substr(filename.lastIndexOf(".")) : "";
+            if (
+                !fileext ||
+                (allowFiles &&
+                    (allowFiles.join("") + ".").indexOf(fileext.toLowerCase() + ".") === -1)
+            ) {
+                UE.dialog.removeLoadingPlaceholder(me, loadingId);
+                UE.dialog.tipError(me, me.getLang("autoupload.exceedTypeError"));
+                return;
+            }
 
-      var upload = function(file){
-        const formData = new FormData();
-        formData.append(me.getOpt('imageFieldName'), file, file.name);
-        UE.api.requestAction(me,me.getOpt("imageActionName"),{
-          data:formData
-        }).then(function(res){
-          if('SUCCESS'===res.data.state && res.data.url){
-            const loader = me.document.getElementById(loadingId);
-            domUtils.removeClasses(loader, "loadingclass");
-            const link = me.options.imageUrlPrefix + res.data.url;
-            loader.setAttribute("src", link);
-            loader.setAttribute("_src", link);
-            loader.setAttribute("alt", res.data.original || "");
-            loader.removeAttribute("id");
-            me.fireEvent("contentchange");
-            // 触发上传图片事件
-            me.fireEvent("uploadsuccess", {
-              res: res.data,
-              type: 'image'
-            });
-          }else{
-            UE.dialog.removeLoadingPlaceholder(me, loadingId);
-            UE.dialog.tipError(me, res.data.state);
-          }
-        }).catch(function(err){
-          UE.dialog.removeLoadingPlaceholder(me, loadingId);
-          UE.dialog.tipError(me, err);
+            var upload = function (file) {
+                const formData = new FormData();
+                formData.append(me.getOpt('imageFieldName'), file, file.name);
+                UE.api.requestAction(me, me.getOpt("imageActionName"), {
+                    data: formData
+                }).then(function (res) {
+                    if ('SUCCESS' === res.data.state && res.data.url) {
+                        const loader = me.document.getElementById(loadingId);
+                        domUtils.removeClasses(loader, "uep-loading");
+                        const link = me.options.imageUrlPrefix + res.data.url;
+                        loader.setAttribute("src", link);
+                        loader.setAttribute("_src", link);
+                        loader.setAttribute("alt", res.data.original || "");
+                        loader.removeAttribute("id");
+                        me.fireEvent("contentchange");
+                        // 触发上传图片事件
+                        me.fireEvent("uploadsuccess", {
+                            res: res.data,
+                            type: 'image'
+                        });
+                    } else {
+                        UE.dialog.removeLoadingPlaceholder(me, loadingId);
+                        UE.dialog.tipError(me, res.data.state);
+                    }
+                }).catch(function (err) {
+                    UE.dialog.removeLoadingPlaceholder(me, loadingId);
+                    UE.dialog.tipError(me, err);
+                });
+            };
+            var file = input.files[0];
+            // console.log('file',file);
+            var imageCompressEnable = me.getOpt('imageCompressEnable'),
+                imageMaxSize = me.getOpt('imageMaxSize'),
+                imageCompressBorder = me.getOpt('imageCompressBorder');
+            if (imageCompressEnable) {
+                UE.image.compress(file, {
+                    maxSizeMB: imageMaxSize / 1024 / 1024,
+                    maxWidthOrHeight: imageCompressBorder
+                }).then(function (compressedFile) {
+                    if (me.options.debug) {
+                        console.log('SimpleUpload.CompressImage', (compressedFile.size / file.size * 100).toFixed(2) + '%');
+                    }
+                    upload(compressedFile);
+                }).catch(function (err) {
+                    console.error('SimpleUpload.CompressImage.error', err);
+                    upload(file);
+                });
+            } else {
+                upload(file);
+            }
         });
-      };
-      var file = input.files[0];
-      // console.log('file',file);
-      var imageCompressEnable = me.getOpt('imageCompressEnable'),
-        imageMaxSize = me.getOpt('imageMaxSize'),
-        imageCompressBorder = me.getOpt('imageCompressBorder');
-      if(imageCompressEnable){
-        UE.image.compress(file,{
-          maxSizeMB: imageMaxSize/1024/1024,
-          maxWidthOrHeight: imageCompressBorder
-        }).then(function(compressedFile){
-          if(me.options.debug){
-            console.log('SimpleUpload.CompressImage', (compressedFile.size / file.size * 100).toFixed(2) + '%');
-          }
-          upload(compressedFile);
-        }).catch(function(err){
-          console.error('SimpleUpload.CompressImage.error', err);
-          upload(file);
+
+        var stateTimer;
+        me.addListener("selectionchange", function () {
+            clearTimeout(stateTimer);
+            stateTimer = setTimeout(function () {
+                var state = me.queryCommandState("simpleupload");
+                if (state === -1) {
+                    input.disabled = "disabled";
+                } else {
+                    input.disabled = false;
+                }
+            }, 400);
         });
-      }else{
-        upload(file);
-      }
-    });
-
-    var stateTimer;
-    me.addListener("selectionchange", function () {
-      clearTimeout(stateTimer);
-      stateTimer = setTimeout(function () {
-        var state = me.queryCommandState("simpleupload");
-        if (state === -1) {
-          input.disabled = "disabled";
-        } else {
-          input.disabled = false;
-        }
-      }, 400);
-    });
-    isLoaded = true;
-  }
-
-  return {
-    bindEvents: {
-      ready: function () {
-        //设置loading的样式
-        utils.cssRule(
-          "loading",
-          ".loadingclass{display:inline-block;cursor:default;background: url('" +
-          this.options.themePath +
-          this.options.theme +
-          "/images/loading.gif') no-repeat center center transparent;border-radius:3px;outline:1px solid #EEE;margin-right:1px;height:22px;width:22px;}\n" +
-          ".loaderrorclass{display:inline-block;cursor:default;background: url('" +
-          this.options.themePath +
-          this.options.theme +
-          "/images/loaderror.png') no-repeat center center transparent;border-radius:3px;outline:1px solid #EEE;margin-right:1px;height:22px;width:22px;" +
-          "}",
-          this.document
-        );
-      },
-      /* 初始化简单上传按钮 */
-      simpleuploadbtnready: function (type, container) {
-        containerBtn = container;
-        me.afterConfigReady(initUploadBtn);
-      }
-    },
-    outputRule: function (root) {
-      utils.each(root.getNodesByTagName("img"), function (n) {
-        if (/\b(loaderrorclass)|(bloaderrorclass)\b/.test(n.getAttr("class"))) {
-          n.parentNode.removeChild(n);
-        }
-      });
-    },
-    commands: {
-      simpleupload: {
-        queryCommandState: function () {
-          return isLoaded ? 0 : -1;
-        }
-      }
+        isLoaded = true;
     }
-  };
+
+    return {
+        bindEvents: {
+            ready: function () {
+                //设置loading的样式
+                utils.cssRule(
+                    "loading",
+                    ".uep-loading{display:inline-block;cursor:default;background: url('" +
+                    this.options.themePath +
+                    this.options.theme +
+                    "/images/loading.gif') no-repeat center center transparent;border-radius:3px;outline:1px solid #EEE;margin-right:1px;height:22px;width:22px;}\n" +
+                    ".uep-loading-error{display:inline-block;cursor:default;background: url('" +
+                    this.options.themePath +
+                    this.options.theme +
+                    "/images/loaderror.png') no-repeat center center transparent;border-radius:3px;outline:1px solid #EEE;margin-right:1px;height:22px;width:22px;" +
+                    "}",
+                    this.document
+                );
+            },
+            /* 初始化简单上传按钮 */
+            simpleuploadbtnready: function (type, container) {
+                containerBtn = container;
+                me.afterConfigReady(initUploadBtn);
+            }
+        },
+        outputRule: function (root) {
+            utils.each(root.getNodesByTagName("img"), function (n) {
+                if (/\b(uep\-loading\-error)|(bloaderrorclass)\b/.test(n.getAttr("class"))) {
+                    n.parentNode.removeChild(n);
+                }
+            });
+        },
+        commands: {
+            simpleupload: {
+                queryCommandState: function () {
+                    return isLoaded ? 0 : -1;
+                }
+            }
+        }
+    };
 });
