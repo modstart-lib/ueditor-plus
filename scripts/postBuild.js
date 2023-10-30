de3f8bca81799a94315808178515e0dc9db13f2a
ModStart <modstart@163.com>
2023-10-30 10:21:28 +0800

    优化：增加文件打包时间戳替换

--- scripts/postBuild.js
+++ scripts/postBuild.js
@@ -1,26 +1,37 @@
 const fs = require('fs');
 
+const timestamp = parseInt((new Date().getTime()) / 1000);
+
 // replace js,html,css file content {timestamp} to current timestamp in dist/ and dist-min/
+function replacePlaceholderFile(filePath) {
+    let content = fs.readFileSync(filePath, 'utf8');
+    let changed = false;
+    if (content.indexOf('{timestamp}') > -1) {
+        content = content.replace(/{timestamp}/g, timestamp);
+        console.log('replacePlaceholder {timestamp}', filePath);
+        changed = true;
+    }
+    if (changed) {
+        fs.writeFileSync(filePath, content);
+    }
+}
+
+function replacePlaceholderInDir(dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const filePath = dir + file;
+        const stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            replacePlaceholderFile(filePath);
+        } else if (stat.isDirectory()) {
+            replacePlaceholderInDir(filePath + '/');
+        }
+    });
+}
+
 function replacePlaceholder(dirs) {
-    const timestamp = parseInt((new Date().getTime()) / 1000);
     dirs.forEach(dir => {
-        const files = fs.readdirSync(dir);
-        files.forEach(file => {
-            const filePath = dir + file;
-            const stat = fs.statSync(filePath);
-            if (stat.isFile()) {
-                let content = fs.readFileSync(filePath, 'utf8');
-                let changed = false;
-                if (content.indexOf('{timestamp}') > -1) {
-                    content = content.replace(/{timestamp}/g, timestamp);
-                    console.log('replacePlaceholder {timestamp}', filePath);
-                    changed = true;
-                }
-                if (changed) {
-                    fs.writeFileSync(filePath, content);
-                }
-            }
-        });
+        replacePlaceholderInDir(dir);
     });
 }
 
