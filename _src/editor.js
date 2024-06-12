6c14ffe118317be6401b500b0450eec8def32fb3
ModStart <modstart@163.com>
2024-06-12 14:55:42 +0800

    发布：3.9.0

--- _src/editor.js
+++ _src/editor.js
@@ -10,7 +10,7 @@ window.UE = baidu.editor = {
     instants: {},
     I18N: {},
     _customizeUI: {},
-    version: "3.9.0-beta",
+    version: "3.9.0",
     constants: {
         STATEFUL: {
             DISABLED: -1,
