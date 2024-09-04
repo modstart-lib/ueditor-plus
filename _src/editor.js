6c5ba69f09a817b2f8cb1018f0c5ee0abae46a64
ModStart <modstart@163.com>
2024-09-04 10:21:35 +0800

    升级版本到4.1.0-beta

--- _src/editor.js
+++ _src/editor.js
@@ -10,7 +10,7 @@ window.UE = baidu.editor = {
     instants: {},
     I18N: {},
     _customizeUI: {},
-    version: "4.0.0",
+    version: "4.1.0-beta",
     constants: {
         STATEFUL: {
             DISABLED: -1,
