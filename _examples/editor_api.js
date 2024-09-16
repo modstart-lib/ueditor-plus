b20c37f7bbe4106793939b7e81cb4abaf7ab4f7d
ModStart <modstart@163.com>
2024-09-16 14:15:41 +0800

    增加CDN配置

--- _examples/editor_api.js
+++ _examples/editor_api.js
@@ -127,6 +127,9 @@
             'adapter/message.js',
         ],
         baseURL = '../_src/';
+    if('open-demo.modstart.com'===window.location.host){
+        window.UEDITOR_HOME_URL = 'https://open-demo-cdn.modstart.com/ueditor-plus/'
+    }
     if (window.UEDITOR_HOME_URL) {
         baseURL = window.UEDITOR_HOME_URL + '_src/';
     }
