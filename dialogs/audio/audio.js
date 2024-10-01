bb9245230f0894626d116f19471562ef25f1182e
ModStart <modstart@163.com>
2024-10-01 19:47:19 +0800

    新增：音频上传自定义

--- dialogs/audio/audio.js
+++ dialogs/audio/audio.js
@@ -370,7 +370,7 @@
                 return;
             }
 
-            uploader = _this.uploader = WebUploader.create({
+            var uploaderOption = {
                 pick: {
                     id: '#filePickerReady',
                     label: lang.uploadSelectFile
@@ -382,7 +382,28 @@
                 fileSingleSizeLimit: fileMaxSize,
                 headers: editor.getOpt('serverHeaders') || {},
                 compress: false
-            });
+            };
+
+            if(editor.getOpt('uploadServiceEnable')) {
+                var service = editor.getOpt('uploadService');
+                uploaderOption.customUpload = function (file, callback) {
+                    service.upload('audio', file, {
+                        success: function( res ) {
+                            callback.onSuccess(file, {_raw:JSON.stringify(res)});
+                        },
+                        error: function( err ) {
+                            callback.onError(file, err);
+                        },
+                        progress: function( percent ) {
+                            callback.onProgress(file, percent);
+                        }
+                    }, {
+                        from: 'audio'
+                    });
+                };
+            }
+
+            uploader = _this.uploader = WebUploader.create(uploaderOption);
             uploader.addButton({
                 id: '#filePickerBlock'
             });
