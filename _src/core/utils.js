65dd4e4e2ef1391a13a3867654e6f62ef746f395
ModStart <modstart@163.com>
2024-10-01 19:40:58 +0800

    新增：涂鸦上传上传自定义

--- _src/core/utils.js
+++ _src/core/utils.js
@@ -1096,6 +1096,24 @@ var utils = (UE.utils = {
             return new Function("return " + utils.trim(s || ""))();
         }
     },
+    base64toBlob: function (base64Data, contentType) {
+        contentType = contentType || "";
+        var sliceSize = 1024;
+        var byteCharacters = atob(base64Data);
+        var bytesLength = byteCharacters.length;
+        var slicesCount = Math.ceil(bytesLength / sliceSize);
+        var byteArrays = new Array(slicesCount);
+        for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+            var begin = sliceIndex * sliceSize;
+            var end = Math.min(begin + sliceSize, bytesLength);
+            var bytes = new Array(end - begin);
+            for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+                bytes[i] = byteCharacters[offset].charCodeAt(0);
+            }
+            byteArrays[sliceIndex] = new Uint8Array(bytes);
+        }
+        return new Blob(byteArrays, { type: contentType });
+    },
     json2str: (function () {
         if (window.JSON) {
             return JSON.stringify;
