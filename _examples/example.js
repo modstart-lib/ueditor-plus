83bc9ccf03edfa62743f376d1b3dfb3bf2b13690
git <git@git-scm.com>
2025-04-15 13:37:36 +0800

    优化：清除缓存

---
+++ _examples/example.js
@@ -0,0 +1,4 @@
+if('open-demo.modstart.com'===window.location.host){
+    window.UEDITOR_HOME_URL = 'https://open-demo-cdn.modstart.com/ueditor-plus/'
+    window.UEDITOR_CORS_URL = 'https://open-demo-cdn.modstart.com/ueditor-plus/'
+}
